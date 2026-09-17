7e38b970a0dd80d041d76a9eb9c457668c1a356c
Mukesh Sharma <mukeshsmileboy@gmail.com>
2026-09-17 18:07:59 +0530

    first comitted

---
+++ deployToEc2/src/main/java/com/javazone/deployToEc2/DeployToEc2Application.java
@@ -0,0 +1,13 @@
+package com.javazone.deployToEc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployToEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployToEc2Application.class, args);
+	}
+
+}
---
+++ deployToEc2/src/test/java/com/javazone/deployToEc2/DeployToEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javazone.deployToEc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployToEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
