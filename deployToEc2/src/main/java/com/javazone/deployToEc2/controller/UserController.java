1b33e59158e40f721014e99d8d27b87cf2890fae
mukeshsmileboy <mukeshsmileboy@gmail.com>
2026-09-17 18:20:15 +0530

    commited

--- deployToEc2/src/main/java/com/javazone/deployToEc2/controller/UserController.java
+++ deployToEc2/src/main/java/com/javazone/deployToEc2/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.javazone.deployToEc2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     @GetMapping("/user")
-    public String getUserDetails(String name){
+    public String getUserDetails(@RequestParam String name){
         return name+" You are onboarded !! ";
     }
 }
